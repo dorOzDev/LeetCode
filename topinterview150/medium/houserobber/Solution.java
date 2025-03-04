d4abdcac91595574e8a9c2f566727c8aa7f169bd
dorOzDev <dorzry@gmail.com>
2025-03-04 14:47:50 +0200

    rob - improved solution to O(1) memory and same runtime(O(n))

--- topinterview150/medium/houserobber/Solution.java
+++ topinterview150/medium/houserobber/Solution.java
@@ -1,23 +1,19 @@
 package topinterview150.medium.houserobber;
 
-class Solution {
+public class Solution {
+
     public int rob(int[] nums) {
-        if(nums.length == 0) {
-            return 0;
+
+        int prev = 0;
+        int curr = 0;
+
+
+        for(int i = 0; i < nums.length; ++i) {
+            int tempMax = Math.max(curr, prev + nums[i]);
+            prev = curr;
+            curr = tempMax;
         }
 
-        if(nums.length == 1) {
-            return nums[0];
-        }
-
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for(int i = 2; i < nums.length; ++i) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-
-        return dp[nums.length - 1];
+        return curr;
     }
 }
