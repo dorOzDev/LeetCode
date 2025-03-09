3802d041068319f53c3e8dd8ab87b4e8a31b8da4
dorOzDev <dorzry@gmail.com>
2025-03-09 18:54:19 +0200

    solve with runtime complexity of O(N)

--- topinterview150/medium/twosum167/Solution.java
+++ topinterview150/medium/twosum167/Solution.java
@@ -6,24 +6,20 @@ import java.util.Arrays;
 public class Solution {
 
     public int[] twoSum(int[] numbers, int target) {
-        int[] ans = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
 
-        for(int i = 0; i < numbers.length; ++i) {
-            int elem = target - numbers[i];
-            int index = Arrays.binarySearch(numbers, elem);
-            if(index >= 0) {
-                if(index == i && index > 0 && numbers[index - 1] == elem) {
-                    return new int[] {index + 1, i + 2};
-                } else if(index == i && index < numbers.length - 1 && numbers[index + 1] == elem) {
-                    return new int[] {i + 1, index + 2};
-                }
-
-                return new int[] {i + 1, index + 1};
+        while(left < right) {
+            if(numbers[left] + numbers[right] == target) {
+                return new int[] {left + 1, right + 1};
+            } else if(numbers[left] + numbers[right] > target) {
+                right--;
+            } else if(numbers[left] + numbers[right] < target) {
+                left++;
             }
         }
 
-
-        return ans;
+        return new int[] {-1, -1};
     }
 
     public static void main(String[] args) {
