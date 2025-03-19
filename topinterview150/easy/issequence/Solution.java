595e5f7719e06c7b2cc050302042a13f24480271
dorOzDev <dorzry@gmail.com>
2025-03-19 14:15:15 +0200

    solution to Is Subsequence(392)

---
+++ topinterview150/easy/issequence/Solution.java
@@ -0,0 +1,20 @@
+package topinterview150.easy.issequence;
+
+public class Solution {
+
+    public boolean isSubsequence(String s, String t) {
+        int sIndex = 0;
+
+        for(int i = 0; i < t.length(); ++i) {
+            if(sIndex == s.length()) {
+                return true;
+            }
+
+            if(s.charAt(sIndex) == t.charAt(i)) {
+                sIndex++;
+            }
+        }
+
+        return sIndex == s.length();
+    }
+}
