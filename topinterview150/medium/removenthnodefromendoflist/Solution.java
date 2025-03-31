74d6ab22b5c1661f9e93aad09b5964ef4f1aceee
dorOzDev <dorzry@gmail.com>
2025-03-31 10:58:31 +0300

    Solution to Remove Nth Node From End of List(19) using two pointers keeping n distance between the two. O(N) runtime O(1) memory

--- topinterview150/medium/removenthnodefromendoflist/Solution.java
+++ topinterview150/medium/removenthnodefromendoflist/Solution.java
@@ -2,38 +2,39 @@ package topinterview150.medium.removenthnodefromendoflist;
 
 import utils.ListNode;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        List<ListNode> indexedList = new ArrayList<>();
         ListNode runner = head;
-
+        ListNode slowRunner = head;
+        ListNode prev = slowRunner;
+        int countNodes = 0;
         while(runner != null) {
-            indexedList.add(runner);
+            if(countNodes < n) {
+                countNodes++;
+            } else {
+                prev = slowRunner;
+                slowRunner = slowRunner.next;
+            }
+
             runner = runner.next;
         }
 
-        int indexNodeToRemove = indexedList.size() - n;
+        if(prev == slowRunner) {
+            ListNode ans = prev.next;
+            prev.next = null;
 
-        ListNode toRemoveNode = indexedList.get(indexNodeToRemove);
-
-        if(indexNodeToRemove == 0) {
-            ListNode ans = toRemoveNode.next;
-            toRemoveNode.next = null;
-            return ans;
+            return  ans;
         }
 
-        ListNode prevNode = indexedList.get(indexNodeToRemove - 1);
-        prevNode.next = toRemoveNode.next;
-        toRemoveNode.next = null;
-
+        prev.next = slowRunner.next;
+        slowRunner.next = null;
         return head;
     }
 
     public static void main(String[] args) {
-        new Solution().removeNthFromEnd(ListNode.generateList(List.of(1,2,3,4,5)), 2);
+        new Solution().removeNthFromEnd(ListNode.generateList(List.of(1)), 1);
     }
 }
