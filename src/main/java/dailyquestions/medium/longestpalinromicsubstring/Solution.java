c5ecb2123760d3351ef17ca8072713dda9c7f7ad
dorOzDev <44348538+dorOzDev@users.noreply.github.com>
2025-07-27 14:06:33 +0300

    solution to Intersection of Two Arrays II. Runtime O(N + M), Memory O(N + M). This solution uses map for counting the frequencies in array1. this can be simplified and have better running time with a fixed array of size 100 since this specific question has constraints that each number is in range of 1<={num}<=1000

---
+++ src/main/java/dailyquestions/medium/longestpalinromicsubstring/Solution.java
@@ -0,0 +1,17 @@
+package dailyquestions.medium.longestpalinromicsubstring;
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        String longestPalindrome = null;
+        int end = s.length() - 1;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(end)) {
+            }
+        }
+
+        return longestPalindrome;
+    }
+}
